a63b9e27de3be22bb732509bbdf6dc84ba066e92
Ryan Hausen <ryan.hausen@gmail.com>
2021-12-07 09:42:16 -0800

    using correct branch

--- src/features/data_provider.py
+++ src/features/data_provider.py
@@ -34,50 +34,54 @@ TRAIN_DATA_PATH = os.path.join(DATA_PATH_PROCESSED, "train")
 TEST_DATA_PATH = os.path.join(DATA_PATH_PROCESSED, "test")
 
 
-def open_file_py(parent_dir: str, suffix: str, idx: int):
+def open_file_py(parent_dir:str, suffix:str, idx:int):
     return fits.getdata(
-        os.path.join(parent_dir, f"{idx.numpy().decode('UTF-8')}-{suffix}.fits")
+        os.path.join(
+            parent_dir,
+            f"{idx.numpy().decode('UTF-8')}-{suffix}.fits"
+        )
     )
 
+def grab_files_py(parent_dir:str, instance_mode:str, idx:int):
 
-def grab_files_py(parent_dir: str, instance_mode: str, idx: int):
-
-    if instance_mode == "v1":
+    if instance_mode=="v1":
         fnames = ["flux", "background", "claim_vectors", "claim_maps", "center_of_mass"]
-    elif instance_mode == "v2":
+    elif instance_mode=="v2":
         fnames = ["flux", "background", "claim_maps", "center_of_mass"]
-    elif instance_mode == "v3":
+    elif instance_mode=="v3":
         fnames = ["flux", "background", "claim_vectors", "claim_maps", "center_of_mass"]
-    elif instance_mode == "v4":
+    elif instance_mode=="v4":
         fnames = ["flux", "background", "claim_vectors", "claim_maps", "center_of_mass"]
-    elif instance_mode == "v5":
+    elif instance_mode=="v5":
         fnames = ["flux", "background", "claim_vectors", "claim_maps", "center_of_mass"]
     elif instance_mode in ["v6", "v7", "v8"]:
         fnames = ["flux", "background", "claim_vectors", "claim_maps", "center_of_mass"]
     else:
-        raise ValueError(
-            "instance_mode must be one of ['v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8']"
-        )
+        raise ValueError("instance_mode must be one of ['v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8']")
 
     arrays = list(
         map(
-            lambda suffix: fits.getdata(
-                os.path.join(parent_dir, f"{idx.numpy().decode('UTF-8')}-{suffix}.fits")
-            ),
-            fnames,
+            lambda suffix: fits.getdata(os.path.join(parent_dir, f"{idx.numpy().decode('UTF-8')}-{suffix}.fits")),
+            fnames
         )
     )
 
     return arrays
 
 
-def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
+def get_files(parent_dir:str, instance_mode:str, item_idx:tf.data.Dataset):
 
-    if instance_mode == "v1":
-        (flux, background, claim_vector, claim_map, center_of_mass) = tf.py_function(
+    if instance_mode=="v1":
+        (
+            flux,
+            background,
+            claim_vector,
+            claim_map,
+            center_of_mass
+        ) = tf.py_function(
             partial(grab_files_py, parent_dir, instance_mode),
             inp=[item_idx],
-            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32),
+            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32)
         )
 
         flux.set_shape([256, 256, 4])
@@ -87,13 +91,18 @@ def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
         center_of_mass.set_shape([256, 256, 1])
 
         vals = (flux, background, claim_vector, claim_map, center_of_mass)
-    elif instance_mode == "v2":
+    elif instance_mode=="v2":
         n = 5
 
-        (flux, background, claim_map, center_of_mass) = tf.py_function(
+        (
+            flux,
+            background,
+            claim_map,
+            center_of_mass
+        ) = tf.py_function(
             partial(grab_files_py, parent_dir, instance_mode),
             inp=[item_idx],
-            Tout=(tf.float32, tf.float32, tf.float32, tf.float32),
+            Tout=(tf.float32, tf.float32, tf.float32, tf.float32)
         )
 
         flux.set_shape([256, 256, 4])
@@ -102,11 +111,17 @@ def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
         center_of_mass.set_shape([256, 256, 1])
 
         vals = (flux, background, claim_map, center_of_mass)
-    elif instance_mode == "v3":
-        (flux, background, claim_vector, claim_map, center_of_mass) = tf.py_function(
+    elif instance_mode=="v3":
+        (
+            flux,
+            background,
+            claim_vector,
+            claim_map,
+            center_of_mass
+        ) = tf.py_function(
             partial(grab_files_py, parent_dir, instance_mode),
             inp=[item_idx],
-            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32),
+            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32)
         )
 
         flux.set_shape([256, 256, 4])
@@ -118,10 +133,16 @@ def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
         vals = (flux, background, claim_vector, claim_map, center_of_mass)
     elif instance_mode in ["v4", "v5", "v6"]:
         n = 3
-        (flux, background, claim_vector, claim_map, center_of_mass) = tf.py_function(
+        (
+            flux,
+            background,
+            claim_vector,
+            claim_map,
+            center_of_mass
+        ) = tf.py_function(
             partial(grab_files_py, parent_dir, instance_mode),
             inp=[item_idx],
-            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32),
+            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32)
         )
 
         flux.set_shape([256, 256, 4])
@@ -149,15 +170,21 @@ def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
     elif instance_mode in ["v7", "v8"]:
         n = 3
         n_bands = 1
-        bands = slice(0, 1)
-        (flux, background, claim_vector, claim_map, center_of_mass) = tf.py_function(
+        bands = slice(0,1)
+        (
+            flux,
+            background,
+            claim_vector,
+            claim_map,
+            center_of_mass
+        ) = tf.py_function(
             partial(grab_files_py, parent_dir, instance_mode),
             inp=[item_idx],
-            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32),
+            Tout=(tf.float32, tf.float32, tf.float32, tf.float32, tf.float32)
         )
 
         flux.set_shape([256, 256, 4])
-        flux = flux[:, :, bands]
+        flux = flux[:,:,bands]
 
         background.set_shape([256, 256, 1])
         claim_vector.set_shape([256, 256, n, 2])
@@ -180,8 +207,7 @@ def get_files(parent_dir: str, instance_mode: str, item_idx: tf.data.Dataset):
 
     return vals
 
-
-# @tf.function(experimental_relax_shapes=True)
+#@tf.function(experimental_relax_shapes=True)
 def apply_and_shape(fn, inputs):
     shape = tf.shape(inputs)
     flat_channels = tf.reshape(inputs, [shape[0], shape[1], -1])
@@ -189,14 +215,14 @@ def apply_and_shape(fn, inputs):
     return tf.reshape(fn(flat_channels), shape)
 
 
-# @gin.configurable(allowlist=[
+#@gin.configurable(allowlist=[
 #    "flip_y",
 #    "flip_x",
 #    "translate_y",
 #    "translate_x",
 #    "rotate",
 #    "instance_mode"
-# ])
+#])
 @tf.function
 def augment(
     flux: tf.Tensor,
@@ -204,15 +230,16 @@ def augment(
     claim_vector: tf.Tensor,
     claim_map: tf.Tensor,
     center_of_mass: tf.Tensor,
-    flip_y: bool = True,
-    flip_x: bool = True,
-    translate_y: bool = True,
-    translate_x: bool = True,
-    rotate: bool = True,
-    instance_mode: str = "v7",
+    flip_y: bool=True,
+    flip_x: bool=True,
+    translate_y: bool=True,
+    translate_x: bool=True,
+    rotate: bool=True,
+    instance_mode: str="v7",
 ):
     pi = tf.constant(np.pi, dtype=tf.float32)
     one_eighty = tf.constant(180, dtype=tf.float32)
+
 
     # Vertical Flip - 50/50
     if flip_y:
@@ -225,9 +252,11 @@ def augment(
             claim_map = fn(claim_map)
             center_of_mass = fn(center_of_mass)
 
+
             tmp_y = claim_vector[..., 0] * -1
             tmp_x = claim_vector[..., 1]
             claim_vector = tf.stack([tmp_y, tmp_x], axis=-1)
+
 
     # Horizontal Flip - 50/50
     if flip_x:
@@ -239,17 +268,20 @@ def augment(
             claim_map = fn(claim_map)
             center_of_mass = fn(center_of_mass)
 
+
             claim_vector = fn(claim_vector)
             tmp_y = claim_vector[..., 0]
             tmp_x = claim_vector[..., 1] * -1
             claim_vector = tf.stack([tmp_y, tmp_x], axis=-1)
+
 
     # Vertical Shift - 50/50
     if translate_y:
         if tf.math.greater(tf.random.uniform(shape=[]), 0.5):
             dy = tf.random.uniform([], minval=0, maxval=25, dtype=tf.int32)
             fn = partial(
-                apply_and_shape, partial(tfa.image.translate, translations=[dy, 0])
+                apply_and_shape,
+                partial(tfa.image.translate, translations=[dy, 0])
             )
 
             flux = fn(flux)
@@ -257,13 +289,15 @@ def augment(
             claim_vector = fn(claim_vector)
             claim_map = fn(claim_map)
             center_of_mass = fn(center_of_mass)
+
 
     # Horizontal Shift - 50/50
     if translate_x:
         if tf.math.greater(tf.random.uniform(shape=[]), 0.5):
             dx = tf.random.uniform([], minval=0, maxval=25, dtype=tf.int32)
             fn = partial(
-                apply_and_shape, partial(tfa.image.translate, translations=[0, dx])
+                apply_and_shape,
+                partial(tfa.image.translate, translations=[0, dx])
             )
 
             flux = fn(flux)
@@ -271,6 +305,7 @@ def augment(
             claim_vector = fn(claim_vector)
             claim_map = fn(claim_map)
             center_of_mass = fn(center_of_mass)
+
 
     # Rotate - 50/50
     if rotate:
@@ -278,7 +313,11 @@ def augment(
             theta = tf.random.uniform(shape=[], minval=1, maxval=360)
             rad = theta * pi / one_eighty
 
-            fn = partial(apply_and_shape, partial(tfa.image.rotate, angles=rad),)
+            fn = partial(
+                apply_and_shape,
+                partial(tfa.image.rotate, angles=rad),
+            )
+
 
             flux = fn(flux)
             background = fn(background)
@@ -297,6 +336,7 @@ def augment(
 
             claim_vector = tf.stack([tmp_y, tmp_x], axis=-1)
 
+
     vals = (
         flux,
         background,
@@ -310,16 +350,16 @@ def augment(
 
 @gin.configurable()
 def get_dataset(
-    batch_size: int, instance_mode: str,
+    batch_size:int,
+    instance_mode:str,
 ) -> Tuple[Tuple[tf.data.Dataset, int], Tuple[tf.data.Dataset, int]]:
-    def get_idxs(directory: str):
+
+    def get_idxs(directory:str):
         return list(set([f.split("-")[0] for f in os.listdir(directory)]))
 
     options = tf.data.Options()
     options.experimental_optimization.map_vectorization.enabled = True
-    options.experimental_distribute.auto_shard_policy = (
-        tf.data.experimental.AutoShardPolicy.DATA
-    )
+    options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
 
     # Every training sample is composed of 5 files so divide the dir count by 5
 
@@ -369,9 +409,13 @@ def get_dataset(
     dataset_train = dataset_train.batch(batch_size)
     # print("TRAINING: ", dataset_train)
 
+
+
+
     test_idxs = get_idxs(TEST_DATA_PATH)
     n_test = len(test_idxs)
     test_idxs = tf.data.Dataset.from_tensor_slices(test_idxs)
+
 
     # n_test = batch_size * 10
     # test_idxs = tf.data.Dataset.from_tensor_slices(
@@ -389,21 +433,22 @@ def get_dataset(
         )
         .with_options(options)
         .repeat()
-        # .shuffle(batch_size + 1)
-        # .map(preprocess)
-        # .map(pure_augment)
+        #.shuffle(batch_size + 1)
+        #.map(preprocess)
+        #.map(pure_augment)
         .batch(batch_size)
     )
+
 
     return (
         dataset_train,
         train_batches_per_epoch,
         dataset_test,
-        test_batches_per_epoch,
+        test_batches_per_epoch
     )
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     dataset = get_dataset(1, "v7")
 
     (train, num_train, test, num_test) = dataset
